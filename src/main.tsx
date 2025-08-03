29248fbdf42cf957532e5e3a66e4896a877fd2c2
Phil Stubbs <philipstubbs13@gmail.com>
2025-08-03 18:36:45 -0500

    github finder

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <StrictMode>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
     </StrictMode>
   </QueryClientProvider>
 );
